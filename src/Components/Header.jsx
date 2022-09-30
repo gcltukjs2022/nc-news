62eb54288aaff5c699352b05efedc49e138e2bc6
gcltukjs2022 <gcltukjs2022@gmail.com>
2022-09-30 14:19:22 +0100

    add changes on readme

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 export default function Header() {
   return (
     <div className="header">
-      <img
-        className="img header--img"
-        src="https://leeds.tech/wp-content/uploads/2017/04/logo-northcoders-1024x227.png"
-        alt="northcoders-banner"
-      />
+      <a href="/">
+        <img
+          className="img header--img"
+          src="https://leeds.tech/wp-content/uploads/2017/04/logo-northcoders-1024x227.png"
+          alt="northcoders-banner"
+        />
+      </a>
     </div>
   );
 }
